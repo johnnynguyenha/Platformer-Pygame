13a136b45911dee3f5a0d915c02cb59f2677c852
Johnny Nguyen <johnnynguyenha@csu.fullerton.edu>
2024-09-20 19:24:03 -0700

    first commit

---
+++ code/main.py
@@ -0,0 +1,43 @@
+from settings import * 
+from pytmx.util_pygame import load_pygame
+from random import choice, randint
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Platformer')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        # groups 
+        self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+
+        if choice(['redefine', 'wushiland']) == 'redefine':
+            print (str(randint(0,11)))
+        else:
+            print ('four seasons')
+
+ 
+    def run(self):
+        while self.running:
+            dt = self.clock.tick(FRAMERATE) / 1000 
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False 
+            
+            # update
+            self.all_sprites.update(dt)
+
+            # draw 
+            self.display_surface.fill(BG_COLOR)
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+        
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run() 
